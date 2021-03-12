406ab05d17689c34a67c36a931532e14f6743c54
William Swartzendruber <wswartzendruber@gmail.com>
2021-03-11 21:53:18 -0700

    Correcting unit tests and enhancing vectors.

--- dsp/src/tm/tests.rs
+++ dsp/src/tm/tests.rs
@@ -8,37 +8,37 @@ use super::*;
 #[test]
 fn test_pq_ootf_peak_1_000() {
 
-    let pq_ootf = PqOotf::new(0.1);
+    let pq_ootf = ToneMapper::new(0.1);
 
     for i in 0..1_000 {
-        assert!(pq_ootf.map(i as f64 / 10_000.0) <= 0.1);
+        assert!(pq_ootf.map(i as f64 / 10_000.0) < 0.1);
     }
 
     for i in 1_000..10_000 {
-        assert!(pq_ootf.map(i as f64 / 10_000.0) >= 0.1);
+        assert!(pq_ootf.map(i as f64 / 10_000.0) == 0.1);
     }
 }
 
 #[test]
 fn test_pq_ootf_peak_4_000() {
 
-    let pq_ootf = PqOotf::new(0.4);
+    let pq_ootf = ToneMapper::new(0.4);
 
     for i in 0..4_000 {
-        assert!(pq_ootf.map(i as f64 / 10_000.0) <= 0.1);
+        assert!(pq_ootf.map(i as f64 / 10_000.0) < 0.1);
     }
 
     for i in 4_000..10_000 {
-        assert!(pq_ootf.map(i as f64 / 10_000.0) >= 0.1);
+        assert!(pq_ootf.map(i as f64 / 10_000.0) == 0.1);
     }
 }
 
 #[test]
 fn test_pq_ootf_peak_10_000() {
 
-    let pq_ootf = PqOotf::new(1.0);
+    let pq_ootf = ToneMapper::new(1.0);
 
-    for i in 0..10_000 {
-        assert!(pq_ootf.map(i as f64 / 10_000.0) <= 0.1);
+    for i in 0..9_999 {
+        assert!(pq_ootf.map(i as f64 / 10_000.0) < 0.1);
     }
 }
