037f9073cd36bcddcc0532b7fde5622ca3526ae3
William Swartzendruber <wswartzendruber@gmail.com>
2021-11-23 22:55:24 -0700

    Better RGB<->YXY unit test.

--- dsp/src/pixel/tests.rs
+++ dsp/src/pixel/tests.rs
@@ -13,40 +13,21 @@ const DIFF: f64 = 0.0000000001;
 #[test]
 fn test_rgb_yxy_round_trip() {
 
-    let in_rgb_pixels = vec![
-        RgbPixel {
-            red: 0.0,
-            green: 0.0,
-            blue: 0.0,
-        },
-        RgbPixel {
-            red: 0.5,
-            green: 0.5,
-            blue: 0.5,
-        },
-        RgbPixel {
-            red: 1.0,
-            green: 1.0,
-            blue: 1.0,
-        },
-        RgbPixel {
-            red: 0.0,
-            green: 0.5,
-            blue: 1.0,
-        },
-        RgbPixel {
-            red: 1.0,
-            green: 0.5,
-            blue: 0.0,
-        },
-    ];
+    for r in 0..1_000 {
+        for g in 0..1_000 {
+            for b in 0..1_000 {
 
-    for in_rgb_pixel in in_rgb_pixels.iter() {
+                let in_rgb_pixel = RgbPixel {
+                    red: r as f64 / 1_000.0,
+                    green: g as f64 / 1_000.0,
+                    blue: b as f64 / 1_000.0,
+                };
+                let out_rgb_pixel = in_rgb_pixel.to_yxy().to_rgb();
 
-        let out_rgb_pixel = in_rgb_pixel.to_yxy().to_rgb();
-
-        assert_approx_eq!(in_rgb_pixel.red, out_rgb_pixel.red, DIFF);
-        assert_approx_eq!(in_rgb_pixel.green, out_rgb_pixel.green, DIFF);
-        assert_approx_eq!(in_rgb_pixel.blue, out_rgb_pixel.blue, DIFF);
+                assert_approx_eq!(in_rgb_pixel.red, out_rgb_pixel.red, DIFF);
+                assert_approx_eq!(in_rgb_pixel.green, out_rgb_pixel.green, DIFF);
+                assert_approx_eq!(in_rgb_pixel.blue, out_rgb_pixel.blue, DIFF);
+            }
+        }
     }
 }
