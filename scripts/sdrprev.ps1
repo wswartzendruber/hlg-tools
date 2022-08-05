f525f3903183e567950da7c20217626c25241bf9
William Swartzendruber <wswartzendruber@gmail.com>
2022-08-04 22:43:03 -0600

    Putting a more proper CC0 header in place.

--- scripts/sdrprev.ps1
+++ scripts/sdrprev.ps1
@@ -1,7 +1,11 @@
 #
 # Copyright 2022 William Swartzendruber
 #
-# Any copyright is dedicated to the Public Domain.
+# To the extent possible under law, the person who associated CC0 with this file has waived all
+# copyright and related or neighboring rights to this file.
+#
+# You should have received a copy of the CC0 legalcode along with this work. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
 #
 # SPDX-License-Identifier: CC0-1.0
 #
